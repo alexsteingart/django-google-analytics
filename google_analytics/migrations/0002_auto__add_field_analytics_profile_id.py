dca46d8c6de779d03756482002122add32b880ae
Alex Steingart <alexsteingart@Alexs-iMac.local>
2014-11-06 14:54:56 -0500

    added profileid field

---
+++ google_analytics/migrations/0002_auto__add_field_analytics_profile_id.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Analytics.profile_id'
+        db.add_column(u'google_analytics_analytics', 'profile_id',
+                      self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Analytics.profile_id'
+        db.delete_column(u'google_analytics_analytics', 'profile_id')
+
+
+    models = {
+        u'google_analytics.analytics': {
+            'Meta': {'object_name': 'Analytics'},
+            'analytics_code': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'profile_id': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"})
+        },
+        u'sites.site': {
+            'Meta': {'ordering': "(u'domain',)", 'object_name': 'Site', 'db_table': "u'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['google_analytics']
--- google_analytics/models.py
+++ google_analytics/models.py
@@ -2,9 +2,60 @@ from django.db import models
 from django.conf import settings
 from django.contrib.sites.models import Site
 
+import httplib2
+from oauth2client.django_orm import Storage
+from apiclient.discovery import build
+
+from pickipicki.apps.metrics.models import CredentialsModel
+
 class Analytics(models.Model):
     site = models.ForeignKey(Site)
     analytics_code = models.CharField(blank=True, max_length=100)
+    profile_id = models.CharField(blank=True, null=True, max_length=100)
+    
+    
+    def getProfileId(self, user):
+        
+        storage = Storage(CredentialsModel, 'id', user, 'credential')
+        credential = storage.get()
+        
+        if credential is not None:
+            http = httplib2.Http()
+            http = credential.authorize(http)
+    
+            service = build("analytics", "v3", http=http)
+            
+            accounts = service.management().accounts().list().execute()
+    
+            if accounts.get('items'):
+                # Get the first Google Analytics account
+    
+                for account in accounts.get('items'):
+                    accountId = account.get('id')
+    
+                    # Get a list of all the Web Properties for the first account
+                    code = self.analytics_code
+    
+                    try:
+                        webproperty = service.management().webproperties().get(accountId=accountId,webPropertyId=code).execute()
+    
+                        if webproperty.get('id'):
+                            # Get the first Web Property ID
+                            firstWebpropertyId = webproperty.get('id')
+    
+                            # Get a list of all Views (Profiles) for the first Web Property of the first Account
+                            profiles = service.management().profiles().list(
+                                accountId=accountId,
+                                webPropertyId=firstWebpropertyId).execute()
+    
+                            if profiles.get('items'):
+                                # return the first View (Profile) ID
+                                self.profile_id =  profiles.get('items')[0].get('id')
+                                self.save()
+                    except HttpError:
+                        #web property not part of found accountId
+                        pass
+            
 
     def __unicode__(self):
         return u"%s" % (self.analytics_code)
